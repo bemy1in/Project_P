f7f2e88cb45432f8bb03d9e87c29cf1dbfe1de4c
김동희 <khho119@naver.com>
2024-11-12 16:26:13 +0900

    turtle test

---
+++ test.py
@@ -0,0 +1,15 @@
+#간단한 파이썬 그래픽 프로그램
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+turtle.mainloop()
+
+
